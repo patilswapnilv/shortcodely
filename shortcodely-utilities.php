78b1bf9d6611fce196f4b15435c03c18b60c2b48
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-11-25 19:05:35 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- shortcodely-utilities.php
+++ shortcodely-utilities.php
@@ -11,12 +11,12 @@ function Shortcodely_show_shortcode_widget_possibilities()
     global $_wp_sidebars_widgets;
 
     $sidebars_widgets = $_wp_sidebars_widgets;
-    ksort($sidebars_widgets);  // push inactive down the bottom of the list
+    ksort($sidebars_widgets); // push inactive down the bottom of the list
     $text = '<ul>';
-    foreach ( $sidebars_widgets as $sidebarid => $sidebar ) {
-        if (is_array($sidebar) ) {
+    foreach ($sidebars_widgets as $sidebarid => $sidebar) {
+        if (is_array($sidebar)) {
             $text .= '<li><em>[do_widget_area ' . $sidebarid . ']</em><ul>';
-            foreach ( $sidebar as $i => $w ) {
+            foreach ($sidebar as $i => $w) {
                 $text .= '<li>';
                 $text .= '[do_widget id="' . $w . '"]';
                 $text .= '</li>';
@@ -30,16 +30,16 @@ function Shortcodely_show_shortcode_widget_possibilities()
 }
 
 /*-----------------------------------*/
-function shortcodely_get_widgets_sidebar( $wid ) 
+function shortcodely_get_widgets_sidebar($wid) 
 {
     /* walk through the registered sidebars with a name and find the id - will be something like sidebar-integer.
     take the first one that matches */
     global $_wp_sidebars_widgets;
 
-    foreach ( $_wp_sidebars_widgets as $sidebarid => $sidebar ) {
-        if (is_array($sidebar) ) { // ignore the 'array version' sidebarid that isnt actually a sidebar
-            foreach ( $sidebar as $i => $w ) {
-                if ($w == $wid ) {
+    foreach ($_wp_sidebars_widgets as $sidebarid => $sidebar) {
+        if (is_array($sidebar)) { // ignore the 'array version' sidebarid that isnt actually a sidebar
+            foreach ($sidebar as $i => $w) {
+                if ($w == $wid) {
                     return    $sidebarid;
                 }
             }
@@ -49,14 +49,14 @@ function shortcodely_get_widgets_sidebar( $wid )
     return false; // widget id not in any sidebar
 }
 /*-----------------------------------*/
-function shortcodely_get_sidebar_id( $name ) 
+function shortcodely_get_sidebar_id($name) 
 {
     /* walk through the registered sidebars with a name and find the id - will be something like sidebar-integer.
     take the first one that matches */
     global $wp_registered_sidebars;
 
-    foreach ( $wp_registered_sidebars as $i => $a ) {
-        if ((isset($a['name'])) and ($a['name'] === $name) ) {
+    foreach ($wp_registered_sidebars as $i => $a) {
+        if ((isset($a['name'])) and ($a['name'] === $name)) {
             return $i;
         }
     }
@@ -64,14 +64,14 @@ function shortcodely_get_sidebar_id( $name )
     return false;
 }
 /*-----------------------------------*/
-function shortcodely_get_sidebar_name( $id ) 
+function shortcodely_get_sidebar_name($id) 
 {
     /* dont need anymore ? or at least temporarily */
     /* walk through the registered sidebars with a name and find the id - will be something like sidebar-integer.  take the first one */
     global $wp_registered_sidebars;
-    foreach ( $wp_registered_sidebars as $i => $a ) {
-        if ((isset($a['id'])) and ($a['id'] === $id) ) {
-            if (isset($a['name']) ) {
+    foreach ($wp_registered_sidebars as $i => $a) {
+        if ((isset($a['id'])) and ($a['id'] === $id)) {
+            if (isset($a['name'])) {
                 return $a['name'];
             } else {
                 return $id;
@@ -87,16 +87,16 @@ function shortcodely_check_if_widget_debug()
     global $said;
     // only do these debug if we are logged in and are the administrator
 
-    if (is_admin() ) {
+    if (is_admin()) {
         return false;
     }   // if running in backend, then do not do debug.  20151217
 
-    if (( ! is_user_logged_in()) or ( ! current_user_can('administrator')) ) {
+    if (( ! is_user_logged_in()) or ( ! current_user_can('administrator'))) {
         return false;
     }
 
-    if (isset($_REQUEST['do_widget_debug']) ) {
-        if (empty($said) ) {
+    if (isset($_REQUEST['do_widget_debug'])) {
+        if (empty($said)) {
             $said = true;
         } else {
             return true;
@@ -116,6 +116,9 @@ function shortcodely_check_if_widget_debug()
     }
 }
 /*-----------------------------------*/
+/**
+ * @param string $type
+ */
 function shortcodely_show_widget_debug( $type, $name, $id, $sidebar ) 
 {
     global $wp_registered_sidebars, $wp_registered_widgets, $_wp_sidebars_widgets, $debugcount;
@@ -124,13 +127,13 @@ function shortcodely_show_widget_debug( $type, $name, $id, $sidebar )
     $debug = shortcodely_check_if_widget_debug();
     $text = Shortcodely_show_shortcode_widget_possibilities();
 
-    if ($type == 'empty' ) {
-        if (current_user_can('administrator') ) {
+    if ($type == 'empty') {
+        if (current_user_can('administrator')) {
             $text = '<p>Problem with do_widget shortcode?  Try one of the following:</p>' . $text;
         }
-    } elseif (($type == 'which one') and ($debug) ) {
+    } elseif (($type == 'which one') and ($debug)) {
         $text = '<p>Debug help is on: Is your widget in the widgets_for_shortcodes sidebar?</p>'
-         . $text;
+            . $text;
     }
 
     return $text;
@@ -140,7 +143,7 @@ function shortcodely_save_shortcodes_sidebar()
 {
     // when switching a theme, save the widgets we use for the shortcodes as they are getting overwritten
     $sidebars_widgets = wp_get_sidebars_widgets();
-    if (! empty($sidebars_widgets['widgets_for_shortcodes']) ) {
+    if ( ! empty($sidebars_widgets['widgets_for_shortcodes'])) {
         update_option('sidebars_widgets_for_shortcodes_saved', $sidebars_widgets['widgets_for_shortcodes']);
     } else {  // our shortcodes sidebar is empty  but when to fix ?
     }
@@ -152,7 +155,7 @@ function shortcodely_restore_shortcodes_sidebar()
     global $_wp_sidebars_widgets;
 
     $sidebars_widgets = wp_get_sidebars_widgets();
-    if (empty($sidebars_widgets['widgets_for_shortcodes']) ) {
+    if (empty($sidebars_widgets['widgets_for_shortcodes'])) {
         $sidebars_widgets['widgets_for_shortcodes'] = get_option('sidebars_widgets_for_shortcodes_saved');
         update_option('sidebars_widgets', $sidebars_widgets);
     }
@@ -162,7 +165,7 @@ function shortcodely_upgrade_sidebar()
 {
     // added in 2014 February for compatibility.. keep for how long. till no sites running older versions.?
     $sidebars_widgets = wp_get_sidebars_widgets();
-    if (! empty($sidebars_widgets['Shortcodes']) and empty($sidebars_widgets['widgets_for_shortcodes']) ) {  // we need to upgrade
+    if ( ! empty($sidebars_widgets['Shortcodes']) and empty($sidebars_widgets['widgets_for_shortcodes'])) {  // we need to upgrade
         $sidebars_widgets['widgets_for_shortcodes'] = $sidebars_widgets['Shortcodes'];
         unset($sidebars_widgets['Shortcodes']);
         update_option('sidebars_widgets', $sidebars_widgets);
